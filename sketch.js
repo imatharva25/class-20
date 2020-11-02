218e420011ae514a72dda12a970f0844d8579b93
imatharva25 <73231948+imatharva25@users.noreply.github.com>
2020-11-02 12:56:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(200,200,50,80);
+  fixedRect.shapeColor="green";
+   fixedRect.debug=true;
+
+   movingRect=createSprite(700,200,80,50);
+   movingRect.shapeColor="green";
+   movingRect.debug=true;
+
+   movingRect.velocityX=-3.5;
+   fixedRect.velocityX=3.5;
+}
+
+function draw() {
+  background("yellow"); 
+ createEdgeSprites();
+  
+  if ((movingRect.x-fixedRect.x<=movingRect.width/2+fixedRect.width/2)&&
+    (fixedRect.x-movingRect.x<=movingRect.width/2+fixedRect.width/2))
+    {
+movingRect.velocityX=movingRect.velocityX*(-1);
+fixedRect.velocityX=fixedRect.velocityX*(-1);
+
+
+
+
+    }
+
+
+    if((movingRect.y-fixedRect.y<=movingRect.height/2+fixedRect.height/2)&&
+    (fixedRect.y-movingRect.y<=movingRect.height/2+fixedRect.height/2)){
+      
+      movingRect.velocityY=movingRect.velocityY*(-1);
+      fixedRect.velocityY=fixedRect.velocityY*(-1);
+    
+    }
+
+drawSprites();
+}
